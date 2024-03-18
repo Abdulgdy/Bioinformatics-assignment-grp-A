68eb94ea4aee2509ebe56fe1aef02b9ef92f29bb
Abdulgdy <163293230+Abdulgdy@users.noreply.github.com>
2024-03-18 11:17:30 +0100

    Update Auwaladam.rb

--- Auwaladam.rb
+++ Auwaladam.rb
@@ -1,1 +1,5 @@
-# Define a hash to store your informationinfo = {  name: "Auwal Adam",  email: "auwaladam928@gmail.com",  user_name: "auwaljos1",  area_of_interest: "cancer genomics"}
+# Define a hash to store your informationinfo = { 
+name: "Auwal Adam",  
+email: "auwaladam928@gmail.com",  
+user_name: "auwaljos1",  
+area_of_interest: "cancer genomics"}
