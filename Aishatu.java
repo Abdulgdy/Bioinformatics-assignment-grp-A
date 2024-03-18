32be235585a9e9890765b49c5ade51d03e492dd9
Abdulgdy <163293230+Abdulgdy@users.noreply.github.com>
2024-03-18 11:17:03 +0100

    Update Aishatu.java

--- Aishatu.java
+++ Aishatu.java
@@ -1,1 +1,4 @@
-System.out.println("Name: Aishatu Abdullahi");    System.out.println("Email: Aishatuabdullahi566@gmail.com");    System.out.println("Slack username: @aishatuabdullahi");    System.out.println("Area of interest: genome project");  }
+System.out.println("Name: Aishatu Abdullahi");    
+System.out.println("Email: Aishatuabdullahi566@gmail.com");    
+System.out.println("Slack username: @aishatuabdullahi");    
+System.out.println("Area of interest: genome project");  }
